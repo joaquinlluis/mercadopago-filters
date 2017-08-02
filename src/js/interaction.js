66355693ede3e8fb924a7a1455623572e034bcf1
joaquin lluis <joaquinlluis@MacBook-Pro-de-joaquin.local>
2017-08-02 12:28:10 -0300

    added indentation menu

--- src/js/interaction.js
+++ src/js/interaction.js
@@ -1,9 +1,40 @@
 $(document).ready(function(){
 
-  $('.dd-button').click(function( event){
-    sectionId = $(this).data('trigger');
+  // $('.dd-button').click(function( event){
+  //   sectionId = $(this).data('trigger');
+  //   $('.ui-dropdown__content').removeClass('active');
+  //   $(sectionId).toggleClass('active');
+  // });
+
+  
+  $('.ui-dropdown').click(function(){
     $('.ui-dropdown__content').removeClass('active');
-    $(sectionId).toggleClass('active');
+    $('.ui-dropdown__content', this).addClass('active');
+  });
+
+  $('.ui-list__item').click(function( event){
+
+    event.stopPropagation();
+    var item_text = $(".ui-list__item-text", this).html();
+    // console.log(item_text);
+
+    buttonId = $(this).data('trigger');
+    // console.log(buttonId);
+    $(buttonId).html(item_text);
+    $(buttonId).parent('.dd-button').addClass('selected');
+    $('.ui-dropdown__content').removeClass('active');
+
+    // $('this').text(function(){
+    //   console.log( "You clicked a list item!" );
+    // });
+  });
+
+  $('.ui-list__item').click(function( event){
+
+    event.stopPropagation();
+    $('.ui-list__item').removeClass('selected');
+    $(this).addClass('selected');
+
   });
 
   // $('body').click(function( event){
